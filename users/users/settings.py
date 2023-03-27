5f52dc44604565d6d0e67386320ab53fd69c8a3e
Thebadbeast <129029510+Thebadbeast@users.noreply.github.com>
2023-03-27 11:37:39 +0530

    Update settings.py

--- users/users/settings.py
+++ users/users/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+#STATICFILES_DIRS = [STATIC_DIR,]
 
 #media files
 MEDIA_URLS = 'media/'
